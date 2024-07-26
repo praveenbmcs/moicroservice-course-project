eca84ed834d313a9ff2a1ef210a6eadcad2014f0
praveenbmcs <praveenbmcs@gmail.com>
2024-07-26 21:15:20 +0530

    created versions.tf

---
+++ terraform/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.60.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      owner = "pravsre"
+    }
+  }
+}
